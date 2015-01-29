7699d21948566fa55bd40bd051c21a7f3bfd33f7
Blake Merryman <blakemerryman@users.noreply.github.com>
2015-01-29 13:57:33 -0600

    Updated 1-HelloWorld.swift with instructions

--- 1-HelloWorld.swift
+++ 1-HelloWorld.swift
@@ -1,3 +1,7 @@
+// Run this small program by passing it into the Swift environment. In Terminal:
+//      $ swift /path/to/file/1-HelloWorld.swift
+// This demonstrates how Swift's immediate mode works, which we build upon when creating our scripts.
+
 let groupName  = "CocoaHeads"
 let attendence = 10
 
